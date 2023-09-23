1ca7963ffc996f38893b6119dc372cd957ac6fb7
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-23 16:57:05 +0100

    fix swap

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,12 +1,20 @@
 #include "sort.h"
 
+/**
+ * selection_sort - Selection sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of the array to sort
+ */
 void selection_sort(int *array, size_t size)
 {
-	int i, j, temp, max_index = 0;
+	unsigned long i, j, temp, max_index = 0;
+
+	if (size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = i + 1; j < size - 1; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[max_index])
 				max_index = j;
@@ -14,6 +22,7 @@ void selection_sort(int *array, size_t size)
 		/**Swap elements*/
 		temp = array[i];
 		array[i] = array[max_index];
-		array[max_index] = array[temp];
+		array[max_index] = temp;
+		print_array(array, size);
 	}
 }
