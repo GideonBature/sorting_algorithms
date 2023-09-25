390d590bb78d3d5a8694de8f54a8348c6202249d
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-25 23:32:04 +0100

    counting sort algo

---
+++ 102-counting_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * counting_sort - Counting sort
+ * @array: Array to sort
+ * @size: Size of array
+ */
+void counting_sort(int *array, size_t size)
+{
+	print_array(array, size);
+}
